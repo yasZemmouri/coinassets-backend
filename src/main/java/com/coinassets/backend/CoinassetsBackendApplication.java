ee1faa07f7ca4a3366d8d8b31bec364f42d2f410
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-28 02:24:08 -0500

    first commit

---
+++ src/main/java/com/coinassets/backend/CoinassetsBackendApplication.java
@@ -0,0 +1,19 @@
+package com.coinassets.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.coinassets.models.User;
+
+@SpringBootApplication
+public class CoinassetsBackendApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(CoinassetsBackendApplication.class, args);
+		
+		User yassine = new User(1 ,"Yassine@gmail.com", "admin");
+		System.out.println(yassine);
+	 
+	}
+
+}
