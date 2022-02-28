b06b54409eb70b5029ccc315c826674144f5287a
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-02-28 02:44:52 -0500

    testing new user

--- src/main/java/com/coinassets/backend/CoinassetsBackendApplication.java
+++ src/main/java/com/coinassets/backend/CoinassetsBackendApplication.java
@@ -12,8 +12,9 @@ public class CoinassetsBackendApplication {
 		//SpringApplication.run(CoinassetsBackendApplication.class, args);
 		
 		User yassine = new User(1 ,"Yassine@gmail.com", "admin");
+		User adil = new User(2 ,"adil@gmail.com", "admin2");
 		System.out.println(yassine);
-		System.out.println(yassine);
+		System.out.println(adil);
 	 
 	}
 
